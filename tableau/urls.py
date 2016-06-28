f5d6804ccf400389f82ebb7daa395971f1adcbf4
eureka <eureka@narusec.com>
2016-06-28 21:25:22 +0900

    초기버

--- tableau/urls.py
+++ tableau/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     #url(r'^select_picture/$', views.select_picture, name='result'),
-    url(r'^result/$', views.result, name='result'),
+    url(r'^result/(?P<pic_id>[0-9]\d+)$', views.result, name='result'),
     url(r'^update/$', views.update, name='result'),
 ]
--- tableau/views.py
+++ tableau/views.py
@@ -20,10 +20,30 @@ def update(request):
 
 def index(request):
     script = '''<script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>'''
-    files = Files.objects.values('filename')
+    files = Files.objects.all()
     size = {'width':200, 'height':300}
     return render(request, 'select_picture.html', {'script':script, 'files':files, 'size':size})
 
-def result(request):
-    return render(request, 'result.html')
+def result(request, pic_id):
+    pic_color = Colors.objects.all().filter(file_id=pic_id)
+    pic_color = pic_color.values()[0]
+    pic_color = (pic_color['red'], pic_color['green'], pic_color['blue'],)
+
+
+    personalities = Personality.objects.all()
+    #print(personalities.values())
+    colors = {}
+    for personality in personalities.values():
+        colors[personality['rgb']] = (personality['red'], personality['green'], personality['blue'],)
+
+    euclidean = lambda x, y: (x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2 + (x[2] - y[2]) ** 2
+    distances = {k: euclidean(v, pic_color) for k, v in colors.items()}
+    rgb_hex = min(distances, key=distances.get)
+
+    for val in personalities.values():
+        if val['rgb'] == rgb_hex:
+            result = val['description']
+            break
+
+    return render(request, 'result.html', context={'personality' : result})
 
